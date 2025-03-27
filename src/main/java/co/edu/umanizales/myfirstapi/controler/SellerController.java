42498a39318f0d41ed254a2b44f4a69ded0bfffd
Jaco-1206 <jacobovascoballesteros@gmail.com>
2025-03-26 20:18:48 -0500

    Cambio de codigos postales de todas las ciudades

--- src/main/java/co/edu/umanizales/myfirstapi/controler/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstapi/controler/SellerController.java
@@ -16,17 +16,17 @@ public class SellerController {
         /// Instanciar 5 seller
         Location manizales = new Location("17001", "Manizales");
         Location pereira = new Location( "66001", "Pereira");
-        Location city3 = new Location(3, "Barranquilla");
-        Location city4 = new Location(4, "Medellin");
-        Location city5 = new Location(5, "Pasto");
+        Location barranquilla = new Location("08001", "Barranquilla");
+        Location medellin = new Location("05001", "Medellin");
+        Location pasto = new Location("52001", "Pasto");
 
 
 
         Seller seller1 = new Seller("101", "Juan", "Pérez", 'M', (byte) 30 , manizales);
         Seller seller2 = new Seller("102", "Peter", "Pan", 'M', (byte) 40, pereira);
-        Seller seller3 = new Seller("103", "Oscar", "Acosta", 'M', (byte) 43, city3);
-        Seller seller4 = new Seller("104", "Maria", "Valencia", 'F', (byte) 54, city4);
-        Seller seller5 = new Seller("105", "Mariana", "Cifuentes", 'F', (byte) 27, city5);
+        Seller seller3 = new Seller("103", "Oscar", "Acosta", 'M', (byte) 43, barranquilla);
+        Seller seller4 = new Seller("104", "Maria", "Valencia", 'F', (byte) 54, medellin);
+        Seller seller5 = new Seller("105", "Mariana", "Cifuentes", 'F', (byte) 27, pasto);
         return "vendedores creados correctamente";
     }
 }
